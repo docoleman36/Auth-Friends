2824319d9d35995f12173f3b69dad00db33c4ccf
DJ Coleman <DJ@DJs-MacBook-Pro.local>
2019-10-05 20:59:49 -0700

    Added login and private route

---
+++ friends/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
+import Friends from './components/Friends';
+
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Switch>
+          <PrivateRoute exact path="/protected" component={Friends} />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ friends/src/components/Friends.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Friends = () => {
+  return (
+    <h1>Hello</h1>
+  )
+}
+
+export default Friends;
