d525e4c326138343f0b6ecb6625bcb6d220b35b0
Donald Coleman <donaldcoleman@Donalds-MacBook-Pro.local>
2019-10-06 18:50:34 -0700

    Added get friends

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
 
 const Friends = () => {
+  const [friend, setFriend] = useState([]);
+  
+  const update = () => {
+    axiosWithAuth()
+      .put('/friends/1', {body: {"name": "Jim", "age": 30, "email": "jim@gmail.com"}})
+      .then(res => {
+        console.log(res.data);
+        setFriend(res.data);
+      })
+      .catch(err => console.log(err));
+  }
+   useEffect(() => {
+    axiosWithAuth()
+      .get('/friends')
+      .then(res => {
+        console.log(res.data);
+        setFriend(res.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+
   return (
-    <h1>Hello</h1>
+    <div>
+        <h1>Friends List</h1>
+        {friend.map(friends => (
+            <div key={friends.id} className="card-container">
+                <div>
+                    <h3>{friends.name}</h3>
+                    <p>{friends.age}</p>
+                    <p>{friends.email}</p>
+                    <button onClick={update}>Update a Friend</button>
+                </div>
+            </div>
+        ))
+        }
+    </div>
   )
 }
 
