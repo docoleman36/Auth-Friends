4f92ef0a69d9311d9caca1e6270619a6764dcc3f
DJ Coleman <DJ@DJs-MacBook-Pro.local>
2019-10-06 22:13:54 -0700

    Completed MVP

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,28 +3,27 @@ import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 import Login from './components/Login';
 import PrivateRoute from './components/PrivateRoute';
-import Friends from './components/Friends';
+import Dashboard from './components/Dashboard';
 
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <ul>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/protected">Protected Page</Link>
-          </li>
-        </ul>
-        <Switch>
-          <PrivateRoute exact path="/protected" component={Friends} />
-          <Route path="/login" component={Login} />
-          <Route component={Login} />
-        </Switch>
-      </div>
+      <nav>
+        <h1>My Many Friends</h1>
+        <div className="links">
+          <Link to="/login">Login</Link>
+          <Link to="/dashboard">Dashboard</Link>
+        </div>
+      </nav>
+      <Switch>
+        {" "}
+        {/*Switch will render the first appropriate component in the list. Start with the most restricted and work down*/}
+        <PrivateRoute exact path="/dashboard" component={Dashboard} />
+        <Route path="/login" component={Login} />
+        <Route component={Login} />
+      </Switch>
     </Router>
   );
 }
--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,46 +1,119 @@
-import React, {useEffect, useState} from 'react';
-import { axiosWithAuth } from '../utils/axiosWithAuth';
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
+const Friend = props => {
+  //destructure my data prop for less typing
+  const { name, age, email, id } = props.data;
 
-const Friends = () => {
-  const [friend, setFriend] = useState([]);
-  
-  const update = () => {
+  //declare all states and constants
+  const [editing, setEditing] = useState(false);
+  const [friendValue, setFriendValue] = useState(props.data);
+
+  const handleDelete = () => {
+    //handleDelete changes updating to true
+    //this has effect throughout ternary statements
+    props.setUpdate(true);
     axiosWithAuth()
-      .put('/friends/1', {body: {"name": "Jim", "age": 30, "email": "jim@gmail.com"}})
+      //then makes an axios call using the token
+      //to delete the specific id of the object
+      //per server documentation
+      .delete("/friends/" + id)
       .then(res => {
-        console.log(res.data);
-        setFriend(res.data);
+        //the resolution returns a new array
+        //set the state to new array to trigger a re-render
+        //switch updating back to false
+        props.setList(res.data);
+        props.setUpdate(false);
       })
-      .catch(err => console.log(err));
-  }
-   useEffect(() => {
-    axiosWithAuth()
-      .get('/friends')
-      .then(res => {
-        console.log(res.data);
-        setFriend(res.data);
-      })
-      .catch(err => console.log(err));
-  }, []);
+      .catch(err => {
+        console.log("Error: ", err);
+      });
+  };
 
+  //typical handlechange
+  const handleChange = e => {
+    setFriendValue({ ...friendValue, [e.target.name]: e.target.value });
+  };
+
+  const handleEdit = () => {
+    if (editing === true) {
+      //if the state of editing is true, clicking will do a put
+      //which updates the current object with the id, according
+      //to server docs
+      axiosWithAuth()
+        .put(`/friends/${id}`, friendValue)
+        .then(res => {
+          //resolution is a new array, set it to state for re-render
+          //set editing back to false
+          props.setList(res.data);
+          setEditing(false);
+        })
+        .catch(err => {
+          console.log("Error: ", err);
+        });
+    }
+  };
 
   return (
-    <div>
-        <h1>Friends List</h1>
-        {friend.map(friends => (
-            <div key={friends.id} className="card-container">
-                <div>
-                    <h3>{friends.name}</h3>
-                    <p>{friends.age}</p>
-                    <p>{friends.email}</p>
-                    <button onClick={update}>Update a Friend</button>
-                </div>
-            </div>
-        ))
-        }
+    <div className="friend">
+      {/* depending on the state of editing, h2 may be an input or the given name */}
+      <h2>
+        {editing ? (
+          <input
+            type="text"
+            name="name"
+            value={friendValue.name}
+            onChange={handleChange}
+          />
+        ) : (
+            name
+          )}
+      </h2>
+      <p>
+        {/* depending on the state of editing, p may be an input or the given age */}
+        Age:{" "}
+        {editing ? (
+          <input
+            type="text"
+            name="age"
+            value={friendValue.age}
+            onChange={handleChange}
+          />
+        ) : (
+            age + "yrs old"
+          )}
+      </p>
+      <p>
+        {/* depending on the state of editing, p may be an input or the given email */}
+        Email:{" "}
+        {editing ? (
+          <input
+            type="text"
+            name="email"
+            value={friendValue.email}
+            onChange={handleChange}
+          />
+        ) : (
+            email
+          )}
+      </p>
+      <div className="buttons">
+        <button onClick={handleDelete}>Delete Friend</button>
+        <button
+          onClick={() => {
+            //runs the handle edit function before setting state of editing
+            //this way, a single button can handle both the toggle
+            //of editing, as well as the submit
+            handleEdit();
+            setEditing(true);
+          }}
+        >
+          {/* button text changes to reflect which one it will do on click */}
+          {editing ? "Submit Changes" : "Edit Friend"}
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Friends;
+export default Friend;
